fcf59655d9b6f61f139a24f4dcb966b4507ed6dd
Rezwan Ahmed <rez1.ahmed007@gmail.com>
2025-03-30 00:29:15 +0600

    Add project files.

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,81 @@
+﻿using System.Web.Mvc;
+using TaskManagementApp.Data;
+using TaskManagementApp.Models;
+
+namespace TaskManagementApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        private DatabaseHelper dbHelper = new DatabaseHelper();
+
+        // GET: Task
+        public ActionResult Index()
+        {
+            var tasks = dbHelper.GetAllTasks();
+            return View(tasks);
+        }
+
+        // GET: Task/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Task/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(TaskItem task)
+        {
+            if (ModelState.IsValid)
+            {
+                dbHelper.AddTask(task);
+                return RedirectToAction("Index");
+            }
+            return View(task);
+        }
+
+        // GET: Task/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var task = dbHelper.GetTaskById(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // POST: Task/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(TaskItem task)
+        {
+            if (ModelState.IsValid)
+            {
+                dbHelper.UpdateTask(task);
+                return RedirectToAction("Index");
+            }
+            return View(task);
+        }
+
+        // GET: Task/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var task = dbHelper.GetTaskById(id);
+            if (task == null)
+            {
+                return HttpNotFound();
+            }
+            return View(task);
+        }
+
+        // POST: Task/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            dbHelper.DeleteTask(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Models/DatabaseHelper.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+using TaskManagementApp.Models;
+
+namespace TaskManagementApp.Data
+{
+    public class DatabaseHelper
+    {
+        
+        private string connectionString = ConfigurationManager.ConnectionStrings["TaskManagementDb"].ConnectionString;
+
+        public List<TaskItem> GetAllTasks()
+        {
+            List<TaskItem> tasks = new List<TaskItem>();
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM TaskItems", connection);
+                SqlDataReader reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    tasks.Add(new TaskItem
+                    {
+                        Id = (int)reader["Id"],
+                        Title = reader["Title"].ToString(),
+                        Description = reader["Description"].ToString(),
+                        IsCompleted = (bool)reader["IsCompleted"]
+                    });
+                }
+            }
+            return tasks;
+        }
+
+        public TaskItem GetTaskById(int id)
+        {
+            TaskItem task = null;
+
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("SELECT * FROM TaskItems WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                SqlDataReader reader = command.ExecuteReader();
+
+                if (reader.Read())
+                {
+                    task = new TaskItem
+                    {
+                        Id = (int)reader["Id"],
+                        Title = reader["Title"].ToString(),
+                        Description = reader["Description"].ToString(),
+                        IsCompleted = (bool)reader["IsCompleted"]
+                    };
+                }
+            }
+            return task;
+        }
+
+        public void AddTask(TaskItem task)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("INSERT INTO TaskItems (Title, Description, IsCompleted) VALUES (@Title, @Description, @IsCompleted)", connection);
+                command.Parameters.AddWithValue("@Title", task.Title);
+                command.Parameters.AddWithValue("@Description", task.Description);
+                command.Parameters.AddWithValue("@IsCompleted", task.IsCompleted);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void UpdateTask(TaskItem task)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("UPDATE TaskItems SET Title = @Title, Description = @Description, IsCompleted = @IsCompleted WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", task.Id);
+                command.Parameters.AddWithValue("@Title", task.Title);
+                command.Parameters.AddWithValue("@Description", task.Description);
+                command.Parameters.AddWithValue("@IsCompleted", task.IsCompleted);
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void DeleteTask(int id)
+        {
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                connection.Open();
+                SqlCommand command = new SqlCommand("DELETE FROM TaskItems WHERE Id = @Id", connection);
+                command.Parameters.AddWithValue("@Id", id);
+                command.ExecuteNonQuery();
+            }
+        }
+    }
+}
